acbbf14db66a6087b61530aa14f35cf212fdccac
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-06-20 19:24:39 +1000

    Add matplotlib section to check_env

--- scipy-2014/check_env.py
+++ scipy-2014/check_env.py
@@ -1,3 +1,4 @@
+# Check skimage along with major dependencies: numpy, scipy, PyQt
 import skimage
 test_failed = bool(skimage.test_verbose())
 doctest_failed = bool(skimage.doctest_verbose())
@@ -6,5 +7,14 @@ if test_failed or doctest_failed:
           "See test output for more details.")
 skimver = int(skimage.__version__.split('.')[1].rstrip('dev'))
 if skimver < 10:
-    print("skimage version 0.10 or higher required, you have 0.%i." % skimver)
+    print("ERROR: skimage version 0.10 or higher required. You have 0.%i." %
+          skimver)
 
+# check matplotlib
+import matplotlib as mpl
+mplver_str = mpl.__version__
+mplver = mplver_str.split('.')
+major, minor = map(int, mplver[:2])
+if major < 1 or (major >= 1 and minor < 2):
+    print("ERROR: matplotlib version 1.2 or higher required. You have %s." %
+          mplver_str)
