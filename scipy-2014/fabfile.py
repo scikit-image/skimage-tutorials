72e80b31e3fea8bd307469f59328bcab435c1444
Tony S Yu <tsyu80@gmail.com>
2014-07-03 23:13:04 -0500

    Update slideshow settings

---
+++ scipy-2014/fabfile.py
@@ -0,0 +1,29 @@
+import fabric.api as fabric
+
+
+NBCONVERT = 'ipython nbconvert {}'
+
+BUILD_SLIDES =  NBCONVERT.format('--to slides {}')
+
+NOTEBOOKS = ['0_color_and_exposure.ipynb',
+             '1_image_filters.ipynb',
+             '2_feature_detection.ipynb']
+
+
+@fabric.task
+def build_slides(exclude=None):
+    exclude = exclude or []
+    filtered_notebooks = (nb for nb in NOTEBOOKS if nb not in exclude)
+    for nb in filtered_notebooks:
+        fabric.local(BUILD_SLIDES.format(nb))
+
+
+@fabric.task
+def slideshow(nb=0):
+    try:
+        nb = NOTEBOOKS[int(nb)]
+    except ValueError:
+        pass
+    build_slides(exclude=[nb])
+    serve = BUILD_SLIDES.format('--post serve {}')
+    fabric.local(serve.format(nb))
