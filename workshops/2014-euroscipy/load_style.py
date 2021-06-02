9425eef62c15418d4e234b08ff3b395d0de3f63d
jni <jni@users.noreply.github.com>
2021-06-02 00:27:30 +0000

    deploy: ef15ba75cf557634bc7743019f62aa43bf5a2ecc

--- workshops/2014-euroscipy/load_style.py
+++
@@ -1,1 +0,0 @@
-../lectures/load_style.py
--- workshops/2014-scipy/soln/edge_angle.py
+++
@@ -1,7 +0,0 @@
-def angle_image(image):
-    image = img_as_float(image)
-    dy = convolve(image, dy_kernel)
-    dx = convolve(image, dx_kernel)
-
-    angle = np.arctan2(dy, dx)
-    return np.degrees(angle)
